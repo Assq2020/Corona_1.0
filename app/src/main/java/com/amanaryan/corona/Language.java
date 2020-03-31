29b3c6477fa991deef499bc161794261d8afcafb
aryan2121 <kayoukaan@gmail.com>
2020-03-31 20:44:41 +0530

    login

--- app/src/main/java/com/amanaryan/corona/Language.java
+++ app/src/main/java/com/amanaryan/corona/Language.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -20,6 +22,7 @@ public class Language extends AppCompatActivity {
 ConstraintLayout poster;
     static final int time=5000;
     static final int time2=4500;
+    static String pnumber;
 static String ChooseLanguage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,29 +32,53 @@ poster=findViewById(R.id.poster);
 
         Animation animation= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_out);
 poster.startAnimation(animation);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-
-                startActivity(new Intent(getApplicationContext(),login.class));
-
-
-            }
-        },time);
-
+LoggedInNotCheck();
     }
 
-    private void godown() {
 
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
+
+    private void LoggedInNotCheck() {
 
 
-            }
-        },time2);
+        SQLiteDatabase conn=openOrCreateDatabase("db",MODE_PRIVATE,null);
+        conn.execSQL("create table if not exists Login(user varchar,RegisteredPhNumber Int);");
+
+
+        Cursor c= conn.rawQuery("select * from Login where user='YES'",null);
+
+
+        if(c.moveToFirst()){
+//static string phnumber
+            pnumber=c.getString(1);
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+
+
+                }
+            },time);
+
+
+        }else{
+//            if user is not logged in then
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+
+                    startActivity(new Intent(getApplicationContext(),login.class));
+
+
+                }
+            },time);
+        }
+
+
+
 
     }
 
--- app/src/main/java/com/amanaryan/corona/login.java
+++ app/src/main/java/com/amanaryan/corona/login.java
@@ -1,26 +1,41 @@
 package com.amanaryan.corona;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.TaskExecutors;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
 
 import java.util.concurrent.TimeUnit;
 
 public class login extends AppCompatActivity {
 ConstraintLayout phoneLy,otpLy;
-Button otpBtn,loginBtn;
-EditText phone,otp;
-
+Button otpBtn,loginBtn,MasterLogin;
+EditText phone,otptext;
+    String phnumber,sentcode;
+    TextView Resendcode;
+    //static ph number is user is already Logged in
+    static String number;
 private FirebaseAuth mAuth;
 
     @Override
@@ -28,33 +43,160 @@ private FirebaseAuth mAuth;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         phoneLy = (ConstraintLayout) findViewById(R.id.phonelay);
-        otpLy = (ConstraintLayout) findViewById(R.id.otplay);
-        otpLy.setVisibility(View.GONE);
-        otpBtn = findViewById(R.id.otpbtn);
-//        loginBtn = findViewById(R.id.loginbtn);
-//        phone = findViewById(R.id.phonId);
-//        otp = findViewById(R.id.otp);
-
+        otpLy=findViewById(R.id.Otplay);
+        otpBtn = findViewById(R.id.Generateotpbtn);
+        loginBtn=findViewById(R.id.Loginbtn);
+        phone=findViewById(R.id.Phonenumberid);
+        otptext=findViewById(R.id.otptext);
+        Resendcode=findViewById(R.id.resendcodeText);
         mAuth = FirebaseAuth.getInstance();
+        MasterLogin=findViewById(R.id.Master_Login);
+//        phoneLy.setVisibility(View.GONE);
+        Resendcode.setVisibility(View.GONE);
+        otpLy.setVisibility(View.GONE);
+        //Auto check weather the user is logged in or not
+//        LoggedInNotCheck();
 
+        Resendcode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestOtp(phnumber);
+
+            }
+        });
+
+
+        //Master Login code
+        MasterLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
+
+        //Request otp to send
         otpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-//                if(phone.getText().toString().isEmpty() && phone.getText().toString().length()==10){
-//                    String phnumber="+91"+phone;
-//                requestOtp(phnumber);
-//                }else{
-//                    phone.setError("Phone number is not Valid");}
+                if( phone.getText().toString().length()==10){
+                     phnumber="+91"+phone.getText().toString();
+                requestOtp(phnumber);
+                    otpLy.setVisibility(View.VISIBLE);
+                    phoneLy.setVisibility(View.GONE);
+                    TwoMinTimerCheck();
+                }else{
+                    phone.setError("Phone number is not Valid");
+                }
 
-                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+//                startActivity(new Intent(getApplicationContext(),MainActivity.class));
             }
         });
+
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                verifySignInCode();
+            }
+        });
+
     }
+//Time check
+    private void TwoMinTimerCheck() {
+    new Handler().postDelayed(new Runnable() {
+        @Override
+        public void run() {
+            Resendcode.setVisibility(View.VISIBLE);
+        }
+    },13000);
+    }
+//
+//    private void LoggedInNotCheck() {
+//
+//
+//        SQLiteDatabase conn=openOrCreateDatabase("db",MODE_PRIVATE,null);
+//        conn.execSQL("create table if not exists Login(user varchar,RegisteredPhNumber Int);");
+//
+//
+//        Cursor c= conn.rawQuery("select * from Login where user='YES'",null);
+//
+//
+//        if(c.moveToFirst()){
+////static string phnumber
+//            number=c.getString(1);
+//            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+//
+//
+//        }else{
+////            if user is not logged in then
+//            phoneLy.setVisibility(View.VISIBLE);
+//        }
+//
+//
+//
+//
+//    }
+
+    private void verifySignInCode() {
+        String codeFromUser=otptext.getText().toString();
+    PhoneAuthCredential credential=PhoneAuthProvider.getCredential(sentcode,codeFromUser);
+  signInWithPhoneAuthCredential(credential);
+    }
+
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential){
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            //successfully signed in as code of user and sent code matched;
+
+
+
+
+                            SQLiteDatabase conn=openOrCreateDatabase("db",MODE_PRIVATE,null);
+                            conn.execSQL("create table if not exists Login(user varchar,RegisteredPhNumber Int);");
+
+                            conn.execSQL("insert into cardcolor values('YES',"+"'"+phnumber+"'"+");");
+                            Toast.makeText(login.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),RegisrationForm.class));
+
+                        }else{
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException){
+
+                                Toast.makeText(login.this, "SignIn Failed!:  Invalid Otp", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    }
+                });
+    }
+
+
+
 
     private void requestOtp(String phnumber) {
-//    PhoneAuthProvider.getInstance().verifyPhoneNumber(phone,120L,TimeUnit.SECONDS,this,mCallbacks);
+    PhoneAuthProvider.getInstance().verifyPhoneNumber(phnumber,120L,TimeUnit.SECONDS,this,mCallbacks);
     }
 
-//    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks=new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+
+        }
+
+        @Override
+        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+            sentcode=s;
+
+        }
+    };
 }
